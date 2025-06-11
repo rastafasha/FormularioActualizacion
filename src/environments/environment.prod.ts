e50be264cc4ce08aa7c9262761422fa9e3912d87
Malcolm Cordova <41023438+rastafasha@users.noreply.github.com>
2025-06-11 09:38:53 -0400

    fix: update API URLs to use the correct domain and adjust base href in index.html

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,8 +3,7 @@ export const environment = {
   //apirest local
   // apiUrl: "http://127.0.0.1:8000/api",
   // apiUrlMedia: "http://127.0.0.1:8000/storage/app/",
-  apiUrl: "https://svcbmf.org/backend-api/public/api",
-  apiUrlMedia: "https://svcbmf.org/backend-api/storage/app/",
-  clientId: 'AQhKPBY5mgg0JustLJCcf6ncmd9RghCiNhXT_b6rNUakyQtnEn8MzCn_dkHAyt5n7_P0Omo5M05to5j0'//malcolm sandbox
-
+  apiUrl: "https://svcbmf.com/backend-api-svcbmf/public/api",
+  apiUrlMedia: "https://svcbmf.com/backend-api-svcbmf/storage/app/",
+  
 };
